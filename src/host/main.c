71b13b86fed0189a8b023e07fa594773ce42e65a
tea1896 <tea1896@gmail.com>
2017-07-11 11:59:10 +0800

    Makefile 修改

--- src/host/main.c
+++ src/host/main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "mpegts/tsInput.h"
+#include "dm8168/dm8168.h"
 
 int main(int argc, char ** argv)
 {
 	printf("******  DM8168 (WELLAV Ltd.) COMPILE TIME :%s %s  ******\n", __DATE__, __TIME__);
 	tsInput_Init();
-	//dm8168_transcode(argc, argv);
+    
+	dm8168_transcode(argc, argv);
 		
 	return 0;
 }
+
+
